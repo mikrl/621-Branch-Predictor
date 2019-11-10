9e6153c57bef1f153f33af54709c3c8887e5da77
Michael <mikrlynch@gmail.com>
2019-11-10 15:17:51 -0500

    Fixed up generating the expt args and opening tempfiles

--- driver/expt_main.cpp
+++ driver/expt_main.cpp
@@ -16,73 +16,71 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
+#include <cstring>
 #include <cstdio>
 #include <unistd.h>
 #include <sys/types.h>
-
-// forks the process 
-pid_t fork_to_tempfile(int pipe_fd[2], std::FILE* tempfile)//, char *arg_array[])
-{
-  pid_t child_proc = fork();
-  if(child_proc>0) // if we are in child process
-    {
-      dup2(pipe_fd[1], fileno(tempfile));
-      close(pipe_fd[0]);
-      close(pipe_fd[1]);
-    }
-  else // if we are parent
-    {
-      close(pipe_fd[0]);
-      close(pipe_fd[1]);
-    }
-  
-  return (child_proc);
-}
+#include <sys/wait.h>
+#include <assert.h>
 
 int main()
 {
   // placeholder array for now. will be populated with
   //{"sim-safe", "-perceptpred", "go.alpha", <GO_ARGS>, NULL, expt#, expt_desc} for example
   std::vector<std::vector<std::string>> experiments{
-						     {"name", "s1", "s2", "s3", "benchmark", "benchmark_args", "END_ARGS", "1", "Test expt1"},
-						     {"name2", "s12", "s22", "s32", "benchmark2", "benchmark_args2", "END_ARGS", "2", "Test expt 2"}};
+						    {"sim-bpred", "-bpred", "nottaken", "-redir:sim", "cc1.alpha", "-O", "1stmt.i", "NULL", "1", "control-nottaken"},
+						     {"sim-bpred", "-bpred", "2lev", "s32","-redir:sim", "cc1.alpha", "-O", "1stmt.i", "NULL", "2", "2-lev"},
+						     {"sim-bpred", "s12", "s22", "s32","-redir:sim", "cc1.alpha", "-O", "1stmt.i", "NULL", "3", "perceptron"}};
+
+  unsigned int MAX_CONCURRENT_JOBS = 10; // how many expts we want running at once max: like a batch, start x, wait for x
+  unsigned int JOBS_RUNNING = 0; // how many jobs are currently active 
+  unsigned int WAITING_ON = 0; // the job we are currently waiting on
   
-  std::cerr << "[*] Welcome to the main driver program.\n"
+  std::cerr << "[*] Welcome to the main driver program" << std::endl
 	    << "[*] This program will fork each experiment and consolidate the results into a file."
 	    << std::endl;
 
   std::vector<pid_t> child_pid_vec;
-  std::vector<std::FILE*> tempfile_vec;
-  int pipe_fd[2];
+  std::vector<std::string> tmpname_vec;
 
-  /* Loop over experiments and exec each one in a new thread, passing the tempfile 
-     to fork_to_tempfile */
+  /* Loop over experiments and exec each one in a new process, passing the tempfile as an arg */
   for (auto arg_vector : experiments)
     {
-      std::vector<char *> expt_arg_vec;
-      char **expt_argv;
-      for (std::string arg_str : arg_vector)
+      char c_tmpname[] = "/tmp/ece621-tempXXXXXX";
+      int tmpfd = mkstemp(c_tmpname); close(tmpfd); // we only care about the filename
+      std::string tmpname (c_tmpname);
+      tmpname_vec.push_back(tmpname); // add c_tmpname to array to maintain order
+      
+      std::vector<char const *> expt_arg_vec; // vec of chars to hold our args
+      char * const* expt_argv; // to hold the data of expt_arg_vec
+
+      for (int arg_idx = 0; arg_idx < arg_vector.size(); arg_idx++)
 	{
-	  std::vector<char> arg(arg_str.c_str(), arg_str.c_str()+arg_str.size()+1);
-	  if (arg_str=="END_ARGS")
+	  if (arg_vector[arg_idx]=="NULL") // if we hit NULL, args are full
 	    {
-	      expt_arg_vec.push_back(&arg[0]);
+	      expt_arg_vec.push_back(nullptr);
 	      break;
 	    }
+	  else
+	    {
+	      expt_arg_vec.push_back(arg_vector[arg_idx].data());
+	      
+	      if (arg_vector[arg_idx] == "-redir:sim") // if arg is output redir
+		{
+		  expt_arg_vec.push_back(tmpname_vec.back().data()); // add tmpfile name as arg
+		}
+	    }
 	}
-	
       pid_t child_proc_pid;
-      std::FILE* tmpf = std::tmpfile(); //create tempfile for process
-      tempfile_vec.push_back(tmpf); // add tempfile to array to maintain order
-      pipe(pipe_fd); // create pipe 
-      expt_argv = expt_arg_vec.data();
+
+      expt_argv = const_cast <char **> (expt_arg_vec.data());
       std::cerr << "[*] Starting experiment " << arg_vector.end()[-2]
 		<< ": " << arg_vector.end()[-1] << std::endl;
       
-      child_proc_pid = fork_to_tempfile(pipe_fd, tmpf);
+      child_proc_pid = fork();
       if (child_proc_pid<0)
 	{ //something went wrong in the fork
-	  //cleanup temp files
 	  std::cerr << "[!!!] Did not fork new process! Exiting." << std::endl; 
 	  exit(1);
 	}
@@ -90,20 +88,49 @@ int main()
 	{
  	  child_pid_vec.push_back(child_proc_pid);
 	  std::cerr << "[*] Forked with PID " << child_proc_pid << std::endl;
+	  JOBS_RUNNING++;
+	  if (JOBS_RUNNING>MAX_CONCURRENT_JOBS)
+	    {
+	      std::cerr << "[*] Job concurrency limit reached. Waiting for the oldest job to finish." << std::endl;
+	      pid_t child_pid = child_pid_vec[WAITING_ON]; // pid of the first unfinished job
+	      waitpid(child_pid, NULL, 0); // wait for it to finish
+	      std::cerr << "[*] Job with PID " << child_pid << " finished." << std::endl;
+	      WAITING_ON++; // move to next unfinished job
+	      JOBS_RUNNING--; // decrement running jobs
+	    }
+
 	}
       else{
-	execv(expt_argv[0], expt_argv);
+	if(execvp(expt_argv[0], expt_argv)){return 1;}
       }
+
     }
-  /*consolidate tempfiles into one */
-  
-  
-  /* cleanup tempfiles */
-  for (auto tempfile : tempfile_vec)
+  std::cerr << "[*] All experiments dispatched." << std::endl;
+  if (WAITING_ON<child_pid_vec.size()-1)
     {
-      std::fclose(tempfile);
+      std::cerr << "[*] Waiting for " << child_pid_vec.size()-WAITING_ON
+		<< " jobs to finish." << std::endl; 
+      for (WAITING_ON; WAITING_ON<child_pid_vec.size(); WAITING_ON++)
+	{
+	  pid_t child_pid = child_pid_vec[WAITING_ON];
+	  waitpid(child_pid, NULL, 0);
+	}
+    }
+  std::cerr << "[*] All experiments finished." << std::endl
+	    << "[*] Consolidating data..." << std::endl
+    ;
+  /*consolidate tempfiles into one and give to python */
+
+  /*------------------------------------------------------*/
+  // Cleanup and quit
+  
+  std::cerr << "[*] Cleaning up tempfiles..." << std::endl;
+  /* cleanup tempfiles */
+  for (auto tempfile : tmpname_vec)
+    {
+      std::remove(tempfile.data());
     }
   
-  
+  std::cerr << "[*] Done!" << std::endl;
   return(0);
 }
